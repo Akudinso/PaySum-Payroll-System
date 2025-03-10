6c9c39e8f8637f91746d9f2084c35bbcce36b930
Akudinso <nwekekingsley0@gmail.com>
2025-03-10 05:50:07 -0700

    added employee.ts

---
+++ src/models/employee.ts
@@ -0,0 +1,36 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from "typeorm";
+import { Payroll } from "./payroll";
+
+@Entity()
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column("decimal", { precision: 10, scale: 2 })
+  salary: number;
+
+  @Column("decimal", { precision: 10, scale: 2, nullable: true })
+  tax: number;
+
+  @Column("decimal", { precision: 10, scale: 2, nullable: true })
+  pension: number;
+
+  @Column("decimal", { precision: 10, scale: 2, nullable: true })
+  nhis: number;
+
+  @OneToMany(() => Payroll, (payroll) => payroll.employee)
+    payrolls: Payroll[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+}
